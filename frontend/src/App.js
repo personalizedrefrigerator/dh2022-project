c44944d70eda6df8436342ada41097d245b146fb
markpock <markpock7@gmail.com>
2022-10-22 15:54:02 -0700

    faulty integration between frontend & backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,32 @@
 import logo from './logo.svg';
+import axios from "axios";
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  function getData() {
+    axios({
+      method: "GET",
+      url:"/posts",
+    })
+    .then((response) => {
+      const res =response.data
+      setData(({
+        name: res.name,
+        content: res.content,
+        tag: res.tag}))
+      console.log(data)
+    }).catch((error) => {
+      if (error.response) {
+        console.log(error.response)
+        console.log(error.response.status)
+        console.log(error.response.headers)
+      }
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +34,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Show Data: </p><button onClick={getData}>Click me</button>
+          {data && <div>
+                <p>Name: {data.name}</p>
+                <p>Content: {data.content}</p>
+                <p>Tag: {data.tag}</p>
+              </div>
+          }
       </header>
     </div>
   );
