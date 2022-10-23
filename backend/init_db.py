c6034fbdf2f6eb75ad4d44f781ae6e19e03e27b4
prado <pradodje@gmail.com>
2022-10-22 20:32:07 -0700

    Add more data to database

--- backend/init_db.py
+++ backend/init_db.py
@@ -10,23 +10,37 @@ with open('db.sql') as file:
 
 cur = connection.cursor()
 
-cur.execute(
+cur.executemany(
     "INSERT INTO user (firstName, lastName, userName, pass, email) VALUES (?, ?, ?, ?, ?)",
-    ('joe', 'p', 'jojo', 'pass1234', 'joe@email.com')
+    [
+        ('Joe', 'P', 'joep', 'pass1234', 'joe@email.com'),
+        ('Mark', 'P', 'markp', 'pass1234', 'joe@email.com'),
+        ('Ayush', 'A', 'ayusha', 'pass1234', 'joe@email.com'),
+        ('Henry', 'H', 'henryh', 'pass1234', 'joe@email.com'),
+        ('Mary', 'S', 'marys', 'pass1234', 'joe@email.com'),
+        ('Suzie', 'Q', 'suzieq', 'pass1234', 'joe@email.com')
+    ]
 )
 
-cur.execute(
+cur.executemany(
     "INSERT INTO tag (tagName, color) VALUES (?, ?)",
-    ('tagName', '#00ff00')
+    [
+        ('UW', '#ff0000'),
+        ('Seattle', '#00ff00'),
+        ('Washington', '#0000ff'),
+        ('USA', '#ff00ff'),
+        ('Safety', '#888888')
+    ]
 )
 
 cur.executemany(
     "INSERT INTO post (title, userId, content, createdDate, tagId) VALUES (?, ?, ?, ?, ?)",
     [
-        ('post-1', 1, 'content-1', '2022-10-22', 1),
-        ('post-2', 1, 'content-2', '2022-10-22', 1),
-        ('post-3', 1, 'content-3', '2022-10-22', 1),
-        ('post-4', 1, 'content-4', '2022-10-22', 1)
+        ('Change UW mascot to a lion', 1, 'I hate dogs, let\'s make the husky a lion!', '2022-10-22', 1),
+        ('We need more parks in Seattle', 2, 'We need more greenery in the city. We should add more parks.', '2022-10-22', 2),
+        ('Change Washington State to Lincoln', 3, 'George Washington sucks. Abe Lincoln was way cooler. Let\'s rename our state.', '2022-10-22', 3),
+        ('Implement rank choice voting for federal elections', 4, 'Rank choice voting is more fair.', '2022-10-22', 4),
+        ('Add more street lights in the Maple Leaf neighborhood', 4, 'It gets really dark at night in Maple Leaf area. It would be great to have more light.', '2022-10-22', 5)
     ]
 )
 
