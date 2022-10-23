8e5e5ae416c91eeeaf5bf3395e1fff4330695a89
markpock <markpock7@gmail.com>
2022-10-23 10:19:51 -0700

    minor changes to post summary styling to get tags

--- frontend/src/post/PostSummary.tsx
+++ frontend/src/post/PostSummary.tsx
@@ -1,11 +1,19 @@
 
 import './PostSummary.css';
 import { PostData } from '../helper/types';
+import Tag from '../header/Tag';
 
 const PostSummary = ({ data }: { data: PostData }) => {
     return (
         <button className='post-summary' type="button">
-            <h2>{data.title}</h2>
+            <div className='postHeader'>
+                <div className='leftDisplay'>
+                    <h2>{data.title}</h2>
+                </div>
+                <div className='rightDisplay'>
+                    <Tag/>
+                </div>
+            </div>
             <p>{data.content}</p>
         </button>
     );
