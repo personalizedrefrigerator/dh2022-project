2f93dc715a680c0aec7f1502738d4128af21b7a9
Henry Heino <personalizedrefrigerator@gmail.com>
2022-10-23 09:10:43 -0700

    Sign out button, sign up button

--- frontend/src/header/Navabar.tsx
+++ frontend/src/header/Navabar.tsx
@@ -1,13 +1,26 @@
 import { Link } from 'react-router-dom';
 import { useToken } from '../helper/loginToken';
+import signOut from '../helper/signOut';
 import './Navabar.css';
 
 const Navabar = () => {
     const profile = <Link to={'profile'} className='navBtn'>Profile</Link>
     const login = <Link to={'login'} className='navBtn'>Login</Link>;
+    const signUp = <Link to={'sign-up'} className='navBtn'>Sign Up</Link>;
+    const signedOutBtns = (
+        <>
+            {login} {signUp}
+        </>
+    );
+    const signedInBtns = (
+        <>
+            {profile}
+            <button onClick={signOut}>Sign Out</button>
+        </>
+    );
 
     const userLoggedIn = useToken() !== null; // Dummy variable, needs changing
-    const rightBtn = userLoggedIn ? profile : login;
+    const rightBtn = userLoggedIn ? signedInBtns : signedOutBtns;
     return (
         <div className='navabar'>
             <div className='left'>
