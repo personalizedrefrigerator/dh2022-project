18c7b4b3478de5f71ea5e2c9f6659b1a0e30f02c
Henry Heino <personalizedrefrigerator@gmail.com>
2022-10-23 10:10:15 -0700

    Add background shadow to tags

--- frontend/src/header/Tag.tsx
+++ frontend/src/header/Tag.tsx
@@ -1,7 +1,7 @@
 import './Searchbar.css'
 
 const Tag = ({ data, bgColor }: { data: string, bgColor: string }) => {
-    const color = {backgroundColor: bgColor}
+    const color = {backgroundColor: bgColor, textShadow: '0px 0px 2px black'}
     return (
         <div style={color} className='tag'>
             {data}
