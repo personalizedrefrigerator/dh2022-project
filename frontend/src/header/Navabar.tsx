b824cb4345f4452354eda8720809fef407e816ae
markpock <markpock7@gmail.com>
2022-10-22 19:26:27 -0700

    Quick navbar and searchbar changes

--- frontend/src/header/Navabar.tsx
+++ frontend/src/header/Navabar.tsx
@@ -2,15 +2,19 @@ import { Link } from 'react-router-dom';
 import './Navabar.css';
 
 const Navabar = () => {
+    const profile = <Link to={'profile'} className='navBtn'>Profile</Link>
+    const login = <Link to={'login'} className='navBtn'>Login</Link>;
+
+    const userLoggedIn = true // Dummy variable, needs changing
+    const rightBtn = userLoggedIn ? profile : login;
     return (
         <div className='navabar'>
             <div className='left'>
                 <Link to={'home'} className='navBtn'>Home</Link>
                 <Link to={'new-post'} className='navBtn'>Create Post</Link>
-                <Link to={'profile'} className='navBtn'>Profile</Link>
             </div>
             <div className='right'>
-                <Link to={'login'} className='navBtn'>Login</Link>
+                {rightBtn}
             </div>
         </div>
     );
--- frontend/src/header/Searchbar.tsx
+++ frontend/src/header/Searchbar.tsx
@@ -1,11 +1,13 @@
+import './Searchbar.css'
+
 const Searchbar = () => {
     return (
-        <>
+        <div className='searchbar'>
             <form>
                 <input></input>
                 <input type="submit" value="Search"/>
             </form>
-        </>
+        </div>
     );
 }
 
