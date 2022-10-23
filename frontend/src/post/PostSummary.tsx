f504e9e658c2ddef2d7c2b11a12711908c431dfa
prado <pradodje@gmail.com>
2022-10-23 09:36:25 -0700

    Add style to post list

--- frontend/src/post/PostSummary.tsx
+++ frontend/src/post/PostSummary.tsx
@@ -4,10 +4,10 @@ import { PostData } from '../helper/types';
 
 const PostSummary = ({ data }: { data: PostData }) => {
     return (
-        <div className='post-summary'>
+        <button className='post-summary' type="button">
             <h2>{data.title}</h2>
             <p>{data.content}</p>
-        </div>
+        </button>
     );
 };
 
