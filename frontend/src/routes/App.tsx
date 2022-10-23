dab029be40b22bc13562341891f17b59b144e887
Henry Heino <personalizedrefrigerator@gmail.com>
2022-10-22 18:46:57 -0700

    CSS styling adjustements

--- frontend/src/routes/App.tsx
+++ frontend/src/routes/App.tsx
@@ -7,7 +7,9 @@ const App = () => {
     return (
         <>
             <Header/>
-            <Outlet/>
+            <main>
+                <Outlet/>
+            </main>
         </>
     );
 };
