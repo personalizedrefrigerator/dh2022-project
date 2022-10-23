1930861042944b28093f3e3b5b005773a7b324b9
prado <pradodje@gmail.com>
2022-10-22 18:27:29 -0700

    Co-authored-by: Henry Heino <personalizedrefrigerator@users.noreply.github.com> Co-authored-by: Mark Pock <markpock@users.noreply.github.com> Co-authored-by: speedstorm1 <speedstorm1@users.noreply.github.com>

--- backend/base.py
+++ backend/base.py
@@ -3,12 +3,30 @@ from flask import Flask
 api = Flask(__name__)
 
 @api.route('/posts')
-def posts_route():
-    response_body = {
-        "name": "Testing...",
-        "content": "Foobar",
-        "tag": "Seattle",
-    }
+def posts():
+    response_body = [];
+    for i in range(10):
+        response_body.append({
+            "id": i,
+            "title": f"post-title-{i}",
+            "author": f"author-{i}",
+            "content": f"this is a post {i}"
+        })
+
     return response_body
 
+@api.route('/users')
+def users():
+    response_body = [];
 
+    for i in range(10):
+        response_body.append({
+            "userid": i,
+            "firstname": f"firstname-{i}",
+            "lastname": f"lastname-{i}",
+            "username": f"username-{i}",
+            "password": f"password-{i}",
+            "email": f"email@email.com-{i}",
+        })
+
+    return response_body
